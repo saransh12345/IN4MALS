b6c140ef4d9a089637a8122e3be85db970bf88de
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-08-30 11:44:08 +0530

    Update index.js

--- src/dept/fashion/index.js
+++ src/dept/fashion/index.js
@@ -26,3 +26,9 @@ var bars = document.getElementById("nav-action");
 			document.querySelector(".back-to-top").style.opacity = "0";
 		}
 	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
