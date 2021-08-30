51ead340ad9cc48f92180585e0c81af5a8b8bef2
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-08-30 11:56:09 +0530

    Update index.js

--- src/dept/geeks/src/index.js
+++ src/dept/geeks/src/index.js
@@ -11,3 +11,24 @@ var bars = document.getElementById("nav-action");
 		bars.classList.toggle('active');
 		nav.classList.toggle('visible');
 	}
+	//Back to top button function
+
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.querySelector(".back-to-top").style.transform = "translateY(0px)";
+			document.querySelector(".back-to-top").style.opacity = "1";
+		} else {
+			document.querySelector(".back-to-top").style.transform = "translateY(1000px)";
+			document.querySelector(".back-to-top").style.opacity = "0";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
